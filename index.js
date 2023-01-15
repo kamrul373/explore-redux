428600f062207c321a5a9b16b911ba76824bc019
kamrul373 <textile.zero@gmail.com>
2023-01-15 10:43:02 -0800

    counter app is configured with reducer, dispatch and store method

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const { createStore } = require("redux")
+
 // constants of type
 const Increment = "INCREMENT"
 const Decrement = "DECREMENT"
+const Reset = "RESET"
 
 // intial state
 const InitialState = {
@@ -17,3 +20,42 @@ const DecrementCount = () => {
         type : Decrement
     }
 }
+const resetCount = () => {
+    return {
+        type : Reset
+    }
+}
+const counterReducer = (state=InitialState,action) => {
+    switch (action.type) {
+        case Increment : return  {
+            ...state ,
+            counter : state.counter + 1
+        }
+        case Decrement : return {
+            ...state,
+            counter : state.counter - 1
+        }
+        case Reset : return {
+            ...state,
+            counter: 0
+        }
+        default : {
+            return state
+        }
+    }
+}
+// store 
+const store = createStore(counterReducer)
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+store.dispatch(IncreaseCount() )
+store.dispatch(IncreaseCount() )
+store.dispatch(IncreaseCount() )
+store.dispatch(IncreaseCount() )
+store.dispatch(DecrementCount() )
+store.dispatch(DecrementCount() )
+store.dispatch(IncreaseCount() )
+store.dispatch(resetCount() )
+
